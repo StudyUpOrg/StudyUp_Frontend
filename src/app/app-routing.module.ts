a462b5630a9ed2fa12cbc2b41a22a5004f1b553e
patrick-fhdw <Patrick.Mnster@edu.fhdw.de>
2023-02-05 23:43:54 +0100

    added studiengänge overview design and small design tweaks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { StudiengangOverviewComponent } from './components/studiengang-overview/studiengang-overview.component';
 
 const routes: Routes = [
   {
-    path: 'login', 
-    component: LoginComponent
-  }
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'studiengänge',
+    component: StudiengangOverviewComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
