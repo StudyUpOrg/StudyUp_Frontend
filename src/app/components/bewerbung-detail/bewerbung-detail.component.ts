f8d4142f1ff31f2245ff2b72e3bf38147891e61e
patrick-fhdw <Patrick.Mnster@edu.fhdw.de>
2023-04-06 17:48:42 +0200

    adjusted logic for file upload and restructured code

--- src/app/components/bewerbung-detail/bewerbung-detail.component.ts
+++ src/app/components/bewerbung-detail/bewerbung-detail.component.ts
@@ -7,74 +7,105 @@ import { StudiengangService } from 'src/app/services/studiengang/studiengang.ser
 import { map } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-bewerbung-detail',
-  templateUrl: './bewerbung-detail.component.html',
-  styleUrls: ['./bewerbung-detail.component.scss'],
+    selector: 'app-bewerbung-detail',
+    templateUrl: './bewerbung-detail.component.html',
+    styleUrls: ['./bewerbung-detail.component.scss'],
 })
 export class BewerbungDetailComponent implements OnInit {
-  public loggedIn!: boolean;
-  public studiengang!: any;
-  public formGroup!: FormGroup;
-  private uploadedFiles!: File[];
-  public uploadedFileNames!: string;
-  public hasBewerbungChangedValidly: boolean = false;
+    public loggedIn!: boolean;
+    public studiengang!: any;
+    public formGroup!: FormGroup;
+    private uploadedFiles!: File[];
+    public uploadedFileNames!: string;
+    public hasBewerbungChangedValidly: boolean = false;
 
-  constructor(
-    private authService: AuthService,
-    private route: ActivatedRoute,
-    private studiengangService: StudiengangService,
-    private bewerbungService: BewerbungService,
-    private formBuilder: FormBuilder
-  ) {}
+    constructor(
+        private authService: AuthService,
+        private route: ActivatedRoute,
+        private studiengangService: StudiengangService,
+        private bewerbungService: BewerbungService,
+        private formBuilder: FormBuilder
+    ) {}
 
-  ngOnInit(): void {
-    this.authService.$loggedIn.subscribe((loggedIn) => {
-      this.loggedIn = loggedIn;
-    });
-    this.route.params.subscribe((params) => {
-      this.getStudiengangAndInitializeFormGroup(params['id']);
-    });
-  }
-
-  private getStudiengangAndInitializeFormGroup(studiengangId: number): void {
-    this.studiengangService
-      .getStudiengangByIdVisitor(studiengangId)
-      .subscribe((studiengang) => {
-        this.studiengang = studiengang;
-        this.formGroup = this.formBuilder.group({
-          applicantName: ['', Validators.required],
-          applicantMail: ['', Validators.required],
-          applicantTelNo: ['', Validators.required]
+    ngOnInit(): void {
+        this.authService.$loggedIn.subscribe((loggedIn) => {
+            this.loggedIn = loggedIn;
         });
-        this.formGroup.valueChanges.subscribe(() => {
-          this.hasBewerbungChangedValidly =
-            this.formGroup.dirty && this.formGroup.valid;
+        this.route.params.subscribe((params) => {
+            this.getStudiengangAndInitializeFormGroup(params['id']);
         });
-      });
-    
-  }
+    }
 
-  public onFilesSelected(event: any): void {
-    this.uploadedFiles = Array.from(event.target.files);
-    this.uploadedFileNames = this.uploadedFiles
-      .map((file) => file.name)
-      .join(', ');
-  }
+    private getStudiengangAndInitializeFormGroup(studiengangId: number): void {
+        this.studiengangService.getStudiengangByIdVisitor(studiengangId).subscribe((studiengang) => {
+            this.studiengang = studiengang;
+            this.formGroup = this.formBuilder.group({
+                applicantFirstName: ['', Validators.required],
+                applicantLastName: ['', Validators.required],
+                applicantMail: ['', Validators.required],
+                applicantTelNo: ['', Validators.required],
+                applicantLetter: ['', Validators.required],
+            });
+            this.formGroup.valueChanges.subscribe(() => {
+                this.hasBewerbungChangedValidly = this.formGroup.dirty && this.formGroup.valid;
+            });
+        });
+    }
 
-  public sendBewerbung(): void {
-    const formValues = this.formGroup.value;
-    const bewerbung: any = {
-      courseId: this.studiengang.course_id,
-      applicantName: formValues.applicantName,
-      applicantMail: formValues.applicantMail,
-      applicantTelNo: formValues.applicantTelNo,
-      applicantLetter: '',
-    };
-    const formDataFiles = new FormData();
+    public onFilesSelected(event: any): void {
+        this.uploadedFiles = Array.from(event.target.files);
+        this.uploadedFileNames = this.uploadedFiles.map((file) => file.name).join(', ');
+    }
 
-    this.uploadedFiles.map((file) => {
-      formDataFiles.append('file', file, file.name);
-    });
-    this.bewerbungService.sendBewerbung(bewerbung, formDataFiles);
-  }
+    public sendBewerbung(): void {
+        const formValues = this.formGroup.value;
+        const bewerbung: any = {
+            courseId: this.studiengang.course_id,
+            applicantFirstName: formValues.applicantFirstName,
+            applicantLastName: formValues.applicantLastName,
+            applicantMail: formValues.applicantMail,
+            applicantTelNo: formValues.applicantTelNo,
+            applicantLetter: formValues.applicantLetter,
+        };
+
+        this.bewerbungService.createBewerbung().subscribe((response) => {
+            if (response.applicationId) {
+                const bewerbungId = response.applicationId;
+                this.bewerbungService.addInformationToBewerbung(bewerbungId, bewerbung).subscribe(() => {
+                    this.uploadedFiles.map((file) => {
+                        const formDataFile = new FormData();
+                        formDataFile.append('file', file, file.name);
+                        if (file == this.uploadedFiles[this.uploadedFiles.length - 1]) {
+                            this.bewerbungService.addFileToBewerbung(bewerbungId, formDataFile).subscribe(() => {
+                                this.bewerbungService.sendBewerbung(bewerbungId).subscribe();
+                            });
+                        } else {
+                            this.bewerbungService.addFileToBewerbung(bewerbungId, formDataFile).subscribe();
+                        }
+                    });
+                });
+            }
+        });
+
+        /*
+        // this.createBewerbung().subscribe((response) => {
+        //     const bewerbungId = response.applicationId;
+        //     this.addInformationToBewerbung(bewerbungId, bewerbung).subscribe(() => {
+        //         bewerbungFiles.map((file) => {
+        //             const formDataFile = new FormData();
+        //             formDataFile.append('file', file, file.name);
+        //             if (file == bewerbungFiles[bewerbungFiles.length - 1]) {
+        //                 this.addFileToBewerbung(bewerbungId, formDataFile).subscribe(() => {
+        //                     this.http
+        //                         .put<any>(this.BACKEND_URL + '/visitor/application/send/' + bewerbungId, {})
+        //                         .subscribe();
+        //                 });
+        //             } else {
+        //                 this.addFileToBewerbung(bewerbungId, formDataFile).subscribe();
+        //             }
+        //         });
+        //     });
+        // });
+        */
+    }
 }
--- src/app/services/bewerbung/bewerbung.service.ts
+++ src/app/services/bewerbung/bewerbung.service.ts
@@ -4,37 +4,26 @@ import { Observable } from 'rxjs';
 import { NotificationService } from '../notification/notification.service';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class BewerbungService {
-  private BACKEND_URL: string = 'https://diedreiprojekt.pythonanywhere.com';
+    private BACKEND_URL: string = 'https://diedreiprojekt.pythonanywhere.com';
 
-  constructor(
-    private http: HttpClient,
-    private notificationService: NotificationService
-  ) {}
+    constructor(private http: HttpClient, private notificationService: NotificationService) {}
 
-  private createBewerbung(): Observable<any> {
-    return this.http.post<number>(this.BACKEND_URL + '/visitor/apply', {});
-  }
+    public createBewerbung(): Observable<any> {
+        return this.http.post<number>(this.BACKEND_URL + '/visitor/apply', {});
+    }
 
-  private addInformationToBewerbung(bewerbungId: number, bewerbung: any): Observable<any> {
-    return this.http.put<any>(this.BACKEND_URL + '/visitor/application/managemant/' + bewerbungId, bewerbung);
-  }
+    public addInformationToBewerbung(bewerbungId: number, bewerbung: any): Observable<any> {
+        return this.http.put<any>(this.BACKEND_URL + '/visitor/application/managemant/' + bewerbungId, bewerbung);
+    }
 
-  private addFilesToBewerbung(bewerbungId: number, files: any): Observable<any> 
-  {
-    return this.http.post<any>(this.BACKEND_URL + '/visitor/application/documents/' + bewerbungId, files);
-  }
+    public addFileToBewerbung(bewerbungId: number, file: FormData): Observable<any> {
+        return this.http.post<any>(this.BACKEND_URL + '/visitor/application/documents/' + bewerbungId, file);
+    }
 
-  public sendBewerbung(bewerbung: any, bewerbungFiles: FormData): void {
-    this.createBewerbung().subscribe(response => {
-      const bewerbungId = response.applicationId;
-      this.addInformationToBewerbung(bewerbungId, bewerbung).subscribe(() => {
-        this.addFilesToBewerbung(bewerbungId, bewerbungFiles).subscribe(() => {
-          this.http.put<any>(this.BACKEND_URL + '/visitor/application/send/' + bewerbungId, {}).subscribe();
-        })
-      })
-    })
-  }
+    public sendBewerbung(bewerbungId: number): Observable<any> {
+        return this.http.put<any>(this.BACKEND_URL + '/visitor/application/send/' + bewerbungId, {});
+    }
 }
