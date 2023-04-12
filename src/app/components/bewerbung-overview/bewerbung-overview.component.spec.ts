6f7e33d7eec3d8517ad6b2cc463a7d603bbe95c9
patrick-fhdw <Patrick.Mnster@edu.fhdw.de>
2023-04-12 22:02:23 +0200

    fixed more linting errors

--- src/app/components/bewerbung-overview/bewerbung-overview.component.spec.ts
+++ src/app/components/bewerbung-overview/bewerbung-overview.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BewerbungOverviewComponent } from './bewerbung-overview.component';
 
 describe('BewerbungOverviewComponent', () => {
-  let component: BewerbungOverviewComponent;
-  let fixture: ComponentFixture<BewerbungOverviewComponent>;
+    let component: BewerbungOverviewComponent;
+    let fixture: ComponentFixture<BewerbungOverviewComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ BewerbungOverviewComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [BewerbungOverviewComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(BewerbungOverviewComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(BewerbungOverviewComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
