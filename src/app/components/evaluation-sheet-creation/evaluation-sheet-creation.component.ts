8dfc442c9ea2652a908b62f13754cbe9424c3eca
patrick-fhdw <Patrick.Mnster@edu.fhdw.de>
2023-04-20 23:41:39 +0200

    merge new evaluation sheet creation into master

--- src/app/components/evaluation-sheet-creation/evaluation-sheet-creation.component.ts
+++ src/app/components/evaluation-sheet-creation/evaluation-sheet-creation.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { StudiengangService } from 'src/app/services/studiengang/studiengang.service';
 
 @Component({
     selector: 'app-evaluation-sheet-creation',
@@ -7,22 +9,56 @@ import { FormControl } from '@angular/forms';
     styleUrls: ['./evaluation-sheet-creation.component.scss'],
 })
 export class EvaluationSheetCreationComponent implements OnInit {
+    public loggedIn!: boolean;
     public criterias!: string[];
-    public studiengangNames!: string[];
-    public studiengangForm!: FormControl;
     public evaluationSheetNameForm!: FormControl;
     public criteriaForm!: FormControl;
+    public studiengaenge!: any[];
+    public displayedColumns!: string[];
 
-    constructor() {}
+    constructor(
+        private studiengangService: StudiengangService,
+        private authService: AuthService
+    ) {}
 
     ngOnInit(): void {
-        this.studiengangNames = [
-            'Marketing',
-            'Angewandte Informatik',
-            'Wirtschaftsinformatik',
-        ];
-        this.studiengangForm = new FormControl();
-        this.evaluationSheetNameForm = new FormControl();
-        this.criteriaForm = new FormControl();
+        this.authService.$loggedIn.subscribe(loggedIn => {
+            this.loggedIn = loggedIn;
+            if (this.loggedIn) {
+                this.getAllStudiengaenge();
+                this.displayedColumns = [
+                    'title',
+                    'degree',
+                    'startDate',
+                    'active',
+                    'checkbox',
+                ];
+                this.criterias = new Array();
+                this.criterias.push('Test1');
+                this.criterias.push('Test2');
+                this.evaluationSheetNameForm = new FormControl();
+                this.criteriaForm = new FormControl();
+            }
+        });
+    }
+
+    public addCriteria(): void {
+        this.criterias.push(this.criteriaForm.value);
+    }
+
+    private getAllStudiengaenge(): void {
+        this.studiengangService
+            .getAllStudiengaengeEmployee()
+            .subscribe((studiengaenge: any) => {
+                studiengaenge.map(
+                    (studiengang: { isSelected: boolean }) =>
+                        (studiengang.isSelected = false)
+                );
+                this.studiengaenge = studiengaenge;
+            });
+    }
+
+    public createEvaluationSheet(): void {
+        console.log(this.studiengaenge);
     }
 }
