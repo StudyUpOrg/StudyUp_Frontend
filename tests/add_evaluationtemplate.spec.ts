3dfad8be9c15f64df98fe9ef5c36e15259db3166
Justin <justin_maximilian.pampus@edu.fhdw.de>
2023-04-23 21:12:22 +0200

    add tests

---
+++ tests/add_evaluationtemplate.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test('add evaluationtemplate', async ({ page }) => {
+  await page.goto('http://localhost:4200/');
+  await page.goto('http://localhost:4200/studiengang/%C3%BCbersicht');
+  await page.locator('a').filter({ hasText: 'Login' }).click();
+  await page.getByLabel('Nutzername *').fill('Justin');
+  await page.getByLabel('Nutzername *').press('Tab');
+  await page.getByLabel('Passwort *').fill('Justin!');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('link', { name: 'Evaluationsbogen erstellen' }).click();
+  await page.getByLabel('Name *').click();
+  await page.getByLabel('Name *').fill('Playwright Test');
+  await page.getByLabel('Kriterium *').click();
+  await page.getByLabel('Kriterium *').fill('Playwright Kriterium');
+  await page.getByRole('button', { name: 'Kriterium hinzufügen' }).click();
+  await page.getByRole('button', { name: 'Evaluationsbogen erstellen' }).click();
+  await expect(page.locator('snack-bar-container')).toHaveText('Evaluationsbogen wurde erfolgreich angelegt')
+});
---
+++ tests/edit_course.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test('edit course', async ({ page }) => {
+  await page.goto('http://localhost:4200/');
+  await page.goto('http://localhost:4200/studiengang/%C3%BCbersicht');
+  await page.locator('a').filter({ hasText: 'Login' }).click();
+  await page.getByLabel('Nutzername *').fill('Justin');
+  await page.getByLabel('Nutzername *').press('Tab');
+  await page.getByLabel('Passwort *').fill('Justin!');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.locator('.mat-cell > a').first().click();
+  await page.locator('#mat-slide-toggle-1 div').first().click();
+  await page.getByRole('button', { name: 'Änderungen speichern' }).click();
+  await expect(page.locator('snack-bar-container')).toHaveText('Der Studiengang wurde erfolgreich aktualisiert')
+  await page.waitForTimeout(6000);
+  await page.locator('#mat-slide-toggle-1 div').first().click();
+  await page.getByRole('button', { name: 'Änderungen speichern' }).click();
+  await expect(page.locator('snack-bar-container')).toHaveText('Der Studiengang wurde erfolgreich aktualisiert')
+});
